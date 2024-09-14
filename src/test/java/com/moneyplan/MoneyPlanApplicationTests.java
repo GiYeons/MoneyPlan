5fd4fde873817fd22780ab1e1f3ef82f4e508203
GiYeons <65033360+GiYeons@users.noreply.github.com>
2024-09-14 18:01:08 +0900

    [env] 초기 환경 세팅

---
+++ src/test/java/com/moneyplan/MoneyPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneyplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
