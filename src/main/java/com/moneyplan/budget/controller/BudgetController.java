e9e9bedd86d2155bd62a96851dafb6347461f65e
GiYeons <65033360+GiYeons@users.noreply.github.com>
2024-09-17 21:07:17 +0900

    [feat] 예산 설정 로직 작성

---
+++ src/main/java/com/moneyplan/budget/controller/BudgetController.java
@@ -0,0 +1,32 @@
+package com.moneyplan.budget.controller;
+
+import com.moneyplan.budget.dto.BudgetCreateReq;
+import com.moneyplan.budget.dto.BudgetCreateRes;
+import com.moneyplan.budget.service.BudgetService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "예산")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/budgets")
+public class BudgetController {
+
+    private final BudgetService budgetService;
+
+    @PostMapping("/")
+    @Operation(summary = "예산 설정", description = "기간별 예산을 설정합니다.")
+    public ResponseEntity<List<BudgetCreateRes>> createBudget(@RequestBody BudgetCreateReq req) {
+        List<BudgetCreateRes> res = budgetService.createBudget(req);
+        return ResponseEntity.ok(res);
+    }
+
+}
---
+++ src/main/java/com/moneyplan/budget/service/BudgetService.java
@@ -0,0 +1,53 @@
+package com.moneyplan.budget.service;
+
+import com.moneyplan.budget.domain.Budget;
+import com.moneyplan.budget.dto.BudgetCreateReq;
+import com.moneyplan.budget.dto.BudgetCreateRes;
+import com.moneyplan.budget.repository.BudgetRepository;
+import com.moneyplan.category.domain.Category;
+import com.moneyplan.category.repository.CategoryRepository;
+import com.moneyplan.common.exception.BusinessException;
+import com.moneyplan.common.exception.ErrorCode;
+import com.moneyplan.member.domain.Member;
+import com.moneyplan.member.repository.MemberRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BudgetService {
+
+    private final CategoryRepository categoryRepository;
+    private final MemberRepository memberRepository;
+    private final BudgetRepository budgetRepository;
+
+    @Transactional
+    public List<BudgetCreateRes> createBudget(BudgetCreateReq req) {
+
+        // 회원가입/로그인 구현 전 임시 코드
+        Member member = memberRepository.findById(1L)
+            .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
+        // 임시 코드 끝
+
+        List<BudgetCreateRes> res = new ArrayList<>();
+
+        req.getCategoryBudgets().forEach((categoryName, amount) -> {
+            if (amount < 0) {
+                throw new BusinessException(ErrorCode.INVALID_AMOUNT_EXCEPTION);
+            }
+            Category category = categoryRepository.findByName(categoryName)
+                .orElseThrow(() -> new BusinessException(ErrorCode.CATEGORY_NOT_FOUND));
+
+            Budget budget = req.toBudget(member, category, amount);
+            budgetRepository.save(budget);
+
+            res.add(BudgetCreateRes.of(budget));
+        });
+
+        return res;
+    }
+}
