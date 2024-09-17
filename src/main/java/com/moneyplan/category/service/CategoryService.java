b00b59cbd915a9708447eeaa7645183cf74a1dd2
GiYeons <65033360+GiYeons@users.noreply.github.com>
2024-09-17 17:16:08 +0900

    [feat] 카테고리 목록 반환 로직 작성

---
+++ src/main/java/com/moneyplan/category/service/CategoryService.java
@@ -0,0 +1,31 @@
+package com.moneyplan.category.service;
+
+import com.moneyplan.category.domain.Category;
+import com.moneyplan.category.dto.CategoryRes;
+import com.moneyplan.category.repository.CategoryRepository;
+import com.moneyplan.common.exception.BusinessException;
+import com.moneyplan.common.exception.ErrorCode;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Transactional
+    public List<CategoryRes> getCategories() {
+
+        List<Category> categories = categoryRepository.findAll();
+
+        if (categories.isEmpty()) {
+            throw new BusinessException(ErrorCode.CATEGORY_NOT_FOUND);
+        }
+        return categories.stream()
+            .map(category -> new CategoryRes(category.getName()))
+            .toList();
+    }
+}
