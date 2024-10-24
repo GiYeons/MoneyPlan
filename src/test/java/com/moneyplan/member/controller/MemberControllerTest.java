cd461f3f040b7d242be6432928962dec229762cb
GiYeons <65033360+GiYeons@users.noreply.github.com>
2024-10-24 16:15:18 +0900

    [test] MemberService 회원가입 성공 테스트코드 작성

---
+++ src/test/java/com/moneyplan/member/controller/MemberControllerTest.java
@@ -0,0 +1,20 @@
+package com.moneyplan.member.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+@DisplayName("MemberContorller 테스트")
+class MemberControllerTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void login() {
+    }
+}
